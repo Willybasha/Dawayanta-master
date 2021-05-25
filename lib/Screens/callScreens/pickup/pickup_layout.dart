2f5851f4b23b71efe08b190eeb7dfc515c617c48
mahmoudSALa <Mssf2017>
2021-05-25 17:31:39 +0200

    4.7.0

---
+++ lib/Screens/callScreens/pickup/pickup_layout.dart
@@ -0,0 +1,42 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:daawyenta/Screens/callScreens/pickup/pickup_sceen.dart';
+import 'package:daawyenta/models/call.dart';
+import 'package:daawyenta/provider/user_provider.dart';
+import 'package:daawyenta/services/call_methods.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class PickupLayout extends StatelessWidget {
+  final Widget scaffold;
+  final CallMethods callMethods = CallMethods();
+
+  PickupLayout({
+    @required this.scaffold,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final UserProvider userProvider = Provider.of<UserProvider>(context);
+
+    return (userProvider != null && userProvider.getUser != null)
+        ? StreamBuilder<DocumentSnapshot>(
+      stream: callMethods.callStream(uid: userProvider.getUser.uid),
+      builder: (context, snapshot) {
+        if (snapshot.hasData && snapshot.data.data != null) {
+          Call call = Call.fromMap(snapshot.data.data());
+
+          if (!call.hasDialled) {
+            return PickupScreen(call: call);
+          }
+        }
+        return scaffold;
+      },
+    )
+        : Scaffold(
+      body: Center(
+        child: CircularProgressIndicator(),
+      ),
+    );
+  }
+}
---
+++ lib/Screens/callScreens/pickup/pickup_sceen.dart
@@ -0,0 +1,79 @@
+import 'package:daawyenta/Screens/chat/widgets/cached_image.dart';
+import 'package:daawyenta/models/call.dart';
+import 'package:daawyenta/services/call_methods.dart';
+import 'package:daawyenta/utils/utils.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import '../call_screen.dart';
+
+class PickupScreen extends StatelessWidget {
+  final Call call;
+  final CallMethods callMethods = CallMethods();
+
+  PickupScreen({
+    @required this.call,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        alignment: Alignment.center,
+        padding: EdgeInsets.symmetric(vertical: 100),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              "Incoming...",
+              style: TextStyle(
+                fontSize: 30,
+              ),
+            ),
+            SizedBox(height: 50),
+            CachedImage(
+              call.callerPic,
+              isRound: true,
+              radius: 180,
+            ),
+            SizedBox(height: 15),
+            Text(
+              call.callerName,
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 20,
+              ),
+            ),
+            SizedBox(height: 75),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                IconButton(
+                  icon: Icon(Icons.call_end),
+                  color: Colors.redAccent,
+                  onPressed: () async {
+                    await callMethods.endCall(call: call);
+                  },
+                ),
+                SizedBox(width: 25),
+                IconButton(
+                  icon: Icon(Icons.call),
+                  color: Colors.green,
+                  onPressed: () async =>
+                  await Permissions.cameraAndMicrophonePermissionsGranted()
+                      ? Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => CallScreen(call: call),
+                    ),
+                  )
+                      : {},
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
